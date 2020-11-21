a3a6799a9e13710da9c760c8c39664bac8ef13f6
Ankita Bantey <38222723+ankitabantey@users.noreply.github.com>
2020-11-21 11:48:02 -0600

    empty map with sample data point

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from 'react'
+import { loadModules } from 'esri-loader';
+import ReactDOM from 'react-dom';
+import { WebMapView } from '../components/WebMapView';
+import { ChakraProvider } from '@chakra-ui/react'
 
-export default function Home() {
-    return (
-        <div>
-            
-        </div>
-    )
+
+import { extendTheme } from "@chakra-ui/react"
+
+export default function Home() {
+   
+    /*Map code starts*/
+    ReactDOM.render(
+        <WebMapView />,
+       document.getElementById('root'),
+   );
+    /*Map code ends*/
+    return (
+        <div>
+            {/*Map code below}
+            <div id="map">
+                <h1> Map </h1>
+            </div>
+            {/*Put other stuff here*/}
+        </div>
+    )
 }
---
+++ client/src/components/WebMapView.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import { loadModules } from 'esri-loader';
+import './WebMapView.css'; 
+
+export class WebMapView extends React.Component {
+  constructor(props) {
+    super(props);
+    this.mapRef = React.createRef();
+  }
+
+  componentDidMount() {
+    // lazy load the required ArcGIS API for JavaScript modules and CSS
+    loadModules(['esri/Map', 'esri/views/MapView', 'esri/Graphic', 'esri/layers/GraphicsLayer'], { css: true })
+    .then(([ArcGISMap, MapView, Graphic, GraphicsLayer]) => {
+      const map = new ArcGISMap({
+        basemap: 'gray-vector'
+      });
+
+      this.view = new MapView({
+        container: this.mapRef.current,
+        map: map,
+        center: [-118, 34],
+        zoom: 8
+      });
+
+      var graphicsLayer = new GraphicsLayer();
+       map.add(graphicsLayer);
+
+       var point = {
+        type: "point",
+        longitude: -118.80657463861,
+        latitude: 34.0005930608889
+      };
+
+      var simpleMarkerSymbol = {
+        type: "simple-marker",
+        color: [226, 119, 40],  // orange
+        outline: {
+          color: [255, 255, 255], // white
+          width: 1
+        }
+      };
+
+      var pointGraphic = new Graphic({
+        geometry: point,
+        symbol: simpleMarkerSymbol
+      });
+
+      graphicsLayer.add(pointGraphic);
+
+    });
+  }
+
+  componentWillUnmount() {
+    if (this.view) {
+      // destroy the map view
+      this.view.destroy();
+    }
+  }
+
+  render() {
+    return (
+      <div className="webmap" ref={this.mapRef} />
+    );
+  }
+}
