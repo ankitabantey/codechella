f683426c585d9b763132141123585226c31de802
Team 1 <samiaa71@gmail.com>
2020-11-22 05:47:52 -0600

    commit UI scroll bar

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -5,6 +5,19 @@ import { Spinner } from '@chakra-ui/react'
 import axios from 'axios'
 import React from 'react'
 import TweetList from '../components/TweetList'
+import './Sidebar.css'
+import TwitterIcon from '@material-ui/icons/Twitter';
+import SidebarOption from './SidebarOption';
+import HomeIcon from '@material-ui/icons/Home';
+import SearchIcon from '@material-ui/icons/Search';
+import NotificationsNoneIcon from '@material-ui/icons/NotificationsNone';
+import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
+import MailOutlineIcon from '@material-ui/icons/MailOutline';
+import BookmarkBorderIcon from '@material-ui/icons/BookmarkBorder';
+import ListAltIcon from '@material-ui/icons/ListAlt';
+import PermIdentityIcon from '@material-ui/icons/PermIdentity';
+import {Button} from "@material-ui/core";
+
 
 
 export default function Home() {
@@ -32,7 +45,25 @@ export default function Home() {
         <Box p={10}>
             <Flex>
                 <Box w='20vw' h='100vh' bg='ghostWhite' mr='10px'>
-                    stuff
+                function Sidebar() {
+  
+                    <div className="sidebar">
+                        {/* Twitter Icon */}
+                        <TwitterIcon className="siderbar__twitterIcon"/>
+                        <SidebarOption active Icon={HomeIcon} text="Home"/>
+                        <SidebarOption Icon={SearchIcon} text="Explore"/>
+                        <SidebarOption Icon={NotificationsNoneIcon} text="Notifications"/>
+                        <SidebarOption Icon={MailOutlineIcon} text="Messages"/>
+                        <SidebarOption Icon={BookmarkBorderIcon} text="Bookmarks"/>
+                        <SidebarOption Icon={ListAltIcon} text="Lists"/>
+                        <SidebarOption Icon={PermIdentityIcon} text="Profile"/>
+                        <SidebarOption Icon={MoreHorizIcon} text="More"/>
+
+                        {/* Button -> Tweet */}
+                        <Button variant="outlined" className="siderbar__tweet" fullWidth>TWEET</Button>
+                    </div>
+
+        }
                 </Box>
                 <Grid
                     h="100vh"
